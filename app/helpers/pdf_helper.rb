89634056d76792fbcd1153cefa4181f0f920a026
Maximilien Bausson <m.bausson@pantographe.studio>
2024-07-25 14:43:14 +0200

    wip

--- app/helpers/pdf_helper.rb
+++ app/helpers/pdf_helper.rb
@@ -4,8 +4,8 @@ module PdfHelper
   def replace_css_variables(css_content)
     css_variables = find_css_variables(css_content)
 
-    css_variables.each do |variable|
-      css_content = css_content.gsub("var(#{variable[:name]})", variable[:value])
+    css_variables.each do |var|
+      css_content = css_content.gsub "var(#{var[:name]})", var[:values].first
     end
 
     css_content
@@ -13,11 +13,26 @@ module PdfHelper
 
   private
 
+  # rubocop:disable Rails/Pluck
   def find_css_variables(css_content)
     tree = Crass.parse(css_content, preserve_comments: false)
-    root_node = tree.find { |e| e[:node] == :style_rule && e[:selector][:value] = :root }
 
-    properties = root_node[:children].filter { |e| e[:node] == :property }
-    properties.filter { |e| e[:name].start_with?("--") }.map { |e| { name: e[:name], value: e[:value] } }
+    selectors = tree.find_all { |e| e[:node] == :style_rule }
+
+    properties = selectors.map { |e| e[:children] }.flatten.filter { |e| e[:node] == :property }
+    variables_declarations = properties.filter { |e| e[:name].start_with?("--") }.group_by { |e| e[:name] }
+
+    variables = []
+
+    variables_declarations.each do |k, v| # rubocop:disable Style/MapIntoArray
+      variables << {
+        name: k,
+        values: v.map { |e| e[:value] },
+        count: v.count
+      }
+    end
+
+    variables
   end
+  # rubocop:enable Rails/Pluck
 end
