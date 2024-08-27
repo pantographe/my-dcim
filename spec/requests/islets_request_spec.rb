a9d47826233318bd1d9e239b3d02f2304173007a
B_Rass <b.rasschaert@pantographe.studio>
2024-08-27 17:54:29 +0200

    Add print tests

--- spec/requests/islets_request_spec.rb
+++ spec/requests/islets_request_spec.rb
@@ -182,4 +182,27 @@ RSpec.describe "Islets" do
       it { expect(response).to redirect_to(islets_path) }
     end
   end
+
+  describe "GET #print" do
+    subject(:response) do
+      get print_islet_path(islet)
+
+      # NOTE: used to simplify usage and custom test done in final spec file.
+      @response # rubocop:disable RSpec/InstanceVariable
+    end
+
+    include_context "with authenticated user"
+
+    context "with not found islet" do
+      let(:islet) { Islet.new(id: SecureRandom.uuid) }
+
+      it { expect { response }.to raise_error(ActiveRecord::RecordNotFound) }
+    end
+
+    context "with existing islet" do
+      it { expect(response).to have_http_status(:success) }
+      it { expect(response).to render_template(:print) }
+      it { expect(response).to render_template("layouts/pdf") }
+    end
+  end
 end
