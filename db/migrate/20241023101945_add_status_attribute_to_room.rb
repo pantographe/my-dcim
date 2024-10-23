6dfe1108f25562ed964be4a1c3424c70b45ad4fd
B_Rass <b.rasschaert@pantographe.studio>
2024-10-23 16:29:24 +0200

    Manage status in views

--- db/migrate/20241023101945_add_status_attribute_to_room.rb
+++ db/migrate/20241023101945_add_status_attribute_to_room.rb
@@ -1,7 +1,20 @@
 # frozen_string_literal: true
 
+class MigrationRoom < ActiveRecord::Base
+  self.table_name = :rooms
+end
+
 class AddStatusAttributeToRoom < ActiveRecord::Migration[7.2]
   def change
-    add_column :rooms, :status, :string
+    add_column :rooms, :status, :integer
+
+    up_only do
+      MigrationRoom.find_each do |room|
+        status = room.display_on_home_page ? 0 : 1
+        room.update(status: status)
+      end
+
+      change_column_null(:rooms, :status, false)
+    end
   end
 end
