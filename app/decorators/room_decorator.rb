65542edebf43ad08676f5679b57e0b3713bbfe53
B_Rass <b.rasschaert@pantographe.studio>
2024-10-24 11:42:38 +0200

    Cleans

--- app/decorators/room_decorator.rb
+++ app/decorators/room_decorator.rb
@@ -2,7 +2,7 @@
 
 class RoomDecorator < ApplicationDecorator
   class << self
-    def status_for_options
+    def statuses_for_options
       Room.statuses.keys.map { |status| [Room.human_attribute_name("status.#{status}"), status] }
     end
   end
