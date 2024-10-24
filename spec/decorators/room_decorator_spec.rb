0b71dce80b1c3747175669dc05d3674fbc26b9e0
B_Rass <b.rasschaert@pantographe.studio>
2024-10-24 11:42:38 +0200

    Cleans

--- spec/decorators/room_decorator_spec.rb
+++ spec/decorators/room_decorator_spec.rb
@@ -3,7 +3,7 @@
 require "rails_helper"
 
 RSpec.describe RoomDecorator, type: :decorator do
-  describe ".status_for_options" do
-    it { expect(described_class.status_for_options.pluck(1)).to match_array(Room.statuses.keys) }
+  describe ".statuses_for_options" do
+    it { expect(described_class.statuses_for_options.pluck(1)).to match_array(Room.statuses.keys) }
   end
 end
