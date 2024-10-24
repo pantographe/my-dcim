8d2555b4b443c877b599b6930659b735faca72cc
B_Rass <b.rasschaert@pantographe.studio>
2024-10-24 12:13:44 +0200

    Add badge for status

--- app/decorators/room_decorator.rb
+++ app/decorators/room_decorator.rb
@@ -6,4 +6,15 @@ class RoomDecorator < ApplicationDecorator
       Room.statuses.keys.map { |status| [Room.human_attribute_name("status.#{status}"), status] }
     end
   end
+
+  def badge_color_for_status
+    case status
+    when "active"
+      "text-bg-success"
+    when "passive"
+      "text-bg-warning"
+    when "planned"
+      "text-bg-primary"
+    end
+  end
 end
--- spec/decorators/room_decorator_spec.rb
+++ spec/decorators/room_decorator_spec.rb
@@ -6,4 +6,30 @@ RSpec.describe RoomDecorator, type: :decorator do
   describe ".statuses_for_options" do
     it { expect(described_class.statuses_for_options.pluck(1)).to match_array(Room.statuses.keys) }
   end
+
+  describe "#badge_color_for_status" do
+    let(:subject) { rooms(:one).decorated }
+
+    context "with status = active" do
+      it { expect(subject.badge_color_for_status).to eq "text-bg-success" }
+    end
+
+    context "with status = passive" do
+      before { rooms(:one).status = :passive }
+
+      it { expect(subject.badge_color_for_status).to eq "text-bg-warning" }
+    end
+
+    context "with status = planned" do
+      before { rooms(:one).status = :planned }
+
+      it { expect(subject.badge_color_for_status).to eq "text-bg-primary" }
+    end
+
+    context "with status = unknown" do
+      before { rooms(:one).status = :unknown }
+
+      it { expect(subject.badge_color_for_status).to be_nil }
+    end
+  end
 end
