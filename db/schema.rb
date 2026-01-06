774ea7172758e6cdc101de2734aa155f317fc4eb
Félix <ffilmont@MacBook-Pro.local>
2026-01-06 18:05:00 +0100

    create migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_12_03_164807) do
+ActiveRecord::Schema[8.0].define(version: 2026_01_06_162540) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -138,7 +138,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_12_03_164807) do
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
     t.integer "modeles_count", default: 0, null: false
-    t.boolean "is_glpi_synchronizable", default: false, null: false
+    t.integer "glpi_sync", default: 0, null: false
   end
 
   create_table "changelog_entries", force: :cascade do |t|
