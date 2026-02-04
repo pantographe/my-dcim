e86898252f98f8ab1c400571cb714ff310650e02
Nicolas Brousse <n.brousse@pantographe.studio>
2026-02-04 19:20:02 +0100

    wip

--- app/models/permission_scope_user.rb
+++ app/models/permission_scope_user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class PermissionScopeUser < ApplicationRecord
-  belongs_to :permission_scope
+  # TODO: how to trigger PermissionScope changelog entry when creating or removing a PermissionScopeUser
+  belongs_to :permission_scope, touch: true
   belongs_to :user
 end
