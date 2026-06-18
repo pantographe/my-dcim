4c53ca8abd8db29f7acfe7fb04a514029d2849b2
Félix Filmont <f.filmont@pantographe.studio>
2026-06-18 11:00:26 +0200

    rename and comment utility function

--- app/controllers/moves_project_steps_controller.rb
+++ app/controllers/moves_project_steps_controller.rb
@@ -56,7 +56,7 @@ class MovesProjectStepsController < ApplicationController
 
   def set_frame_updated
     authorize! @frame = Frame.friendly.find(params[:frame_id]), with: FramePolicy, to: :show?
-    @servers = @moves_project_step.servers_moves_for_frame_at_current_step(@frame)
+    @servers = @moves_project_step.frame_servers_at_current_step_for(@frame)
     @moved_connections = MovedConnection.per_servers(@servers)
     @moves = @moves_project_step.moves.where(frame: @frame, moveable_type: "Server")
   end
